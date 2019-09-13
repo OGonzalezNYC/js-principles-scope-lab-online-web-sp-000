12ba36bf0418ab501844d2d45cfadd3095960154
Omar Gonzalez <ogonzaleznyc@gmail.com>
2019-09-13 01:38:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 // Write your solution in this file!
-var customerName = 'bob';
+// var customerName = 'bob';
 
-function upperCaseCustomerName() {
-  customerName = customerName.toUpperCase()
-}
+// function upperCaseCustomerName() {
+//   customerName = customerName.toUpperCase()
+// }
 
-function setBestCustomer() {
-  bestCustomer = 'not bob'
-}
+// function setBestCustomer() {
+//   bestCustomer = 'not bob'
+// }
 
-function overwriteBestCustomer() {
-  bestCustomer = 'maybe bob'
-}
+// function overwriteBestCustomer() {
+//   bestCustomer = 'maybe bob'
+// }
 
-const leastFavoriteCustomer = 'robert'
+// const leastFavoriteCustomer = 'robert'
 
-function changeLeastFavoriteCustomer() {
-  leastFavoriteCustomer = 'not robert';
-}
+// function changeLeastFavoriteCustomer() {
+//   leastFavoriteCustomer = 'not robert';
+// }
