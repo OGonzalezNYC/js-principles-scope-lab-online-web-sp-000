f8543d4a524d7282a459fbffb792f449ddd4998c
Omar Gonzalez <ogonzaleznyc@gmail.com>
2019-09-13 01:45:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function overwriteBestCustomer() {
   bestCustomer = 'maybe bob';
 }
 
-const leastFavoriteCustomer = ;
+const leastFavoriteCustomer = 'x';
 
 function changeLeastFavoriteCustomer() {
   leastFavoriteCustomer = ;
